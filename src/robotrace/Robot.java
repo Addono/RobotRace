cfa902298745f08f07852d0df346d3c727efc824
jeffry55 <jeffry.mozalbat@hotmail.com>
2015-11-27 21:39:25 +0100

    Revert "Added some comments"
    This reverts commit 7ae03a0b57325edb89d359338f58786039211227.

--- src/robotrace/Robot.java
+++ src/robotrace/Robot.java
@@ -92,7 +92,8 @@ class Robot {
         gl.glPopMatrix();
         
         
-        //Create main part (horizontal part where legs connect to.).
+        //creat main line
+        
         gl.glPushMatrix();
             gl.glTranslatef(legDistance, 0, height);
             gl.glRotatef(-90, 0, 1, 0);
@@ -101,27 +102,28 @@ class Robot {
         gl.glPopMatrix();
         
         
-        //Create triangles (two diagonal parts on both sides).
+        //creat triangles
+        
         float o = height;
         float a = (float) Math.sqrt(2 * Math.pow(legDistance / 2, 2));
         float s = (float) (Math.sqrt(Math.pow(o, 2) + Math.pow(a, 2)));
         float angle = (float) (Math.atan(a / o) * 180 / Math.PI);
         
         for(int j=0; j<=180 ; j+=180){
+        gl.glPushMatrix();
+        gl.glRotatef(j, 0, 0, 1);
+        gl.glTranslatef(legDistance, 0, 0);
+        for(int i = 90; i < 270; i += 90) {
             gl.glPushMatrix();
-            gl.glRotatef(j, 0, 0, 1);
-            gl.glTranslatef(legDistance, 0, 0);
-            for(int i = 90; i < 270; i += 90) {
-                gl.glPushMatrix();
-                    gl.glRotatef(i, 0.0f, 0.0f, 1.0f);
-                    gl.glTranslatef(-.5f * legDistance, -.5f * legDistance, 0f);
-                    gl.glRotatef(45, 0f, 0f, 1f);
-                    gl.glRotatef(angle, 0f, 1f, 0f);
-                    drawPart(gl, glut, shapeWidth, s, stickFigure);
-                gl.glPopMatrix();
-            }
+                gl.glRotatef(i, 0.0f, 0.0f, 1.0f);
+                gl.glTranslatef(-.5f * legDistance, -.5f * legDistance, 0f);
+                gl.glRotatef(45, 0f, 0f, 1f);
+                gl.glRotatef(angle, 0f, 1f, 0f);
+                drawPart(gl, glut, shapeWidth, s, stickFigure);
             gl.glPopMatrix();
         }
+        gl.glPopMatrix();
+    }
         gl.glPopMatrix();
     }
     
