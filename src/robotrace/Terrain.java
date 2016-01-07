d5f13761db8b3bdac526c0e542d594d83679106b
Addono <a.d.knapen@student.tue.nl>
2016-01-07 19:00:46 +0100

    Added more comments, and made the difference between coordinates and points on the plane more clear.

--- src/robotrace/Terrain.java
+++ src/robotrace/Terrain.java
@@ -10,22 +10,22 @@ import javax.media.opengl.glu.GLU;
  * Implementation of the terrain.
  */
 class Terrain {
-    int amount_x = 200;
-    int amount_y = 200;
+    int amount_v = 200;
+    int amount_u = 200;
     float size_x = 40;
     float size_y = 40;
     
-    float[][] heights = new float[amount_x][amount_y]; // Stores all the heights for all the different point.
+    float[][] heights = new float[amount_v][amount_u]; // Stores all the heights for all the different point.
     
     /**
      * Pre-computes the height for all points.
      */
     public Terrain() {
-        for(int x = 0; x < amount_x; x++) {
-            float x_cor = xCoordinate(x);
-            for(int y = 0; y < amount_y; y++) {
-                float y_cor = yCoordinate(y);
-                heights[x][y] = heightAt(x_cor, y_cor);
+        for(int v = 0; v < amount_v; v++) {
+            float x = xCoordinate(v);
+            for(int u = 0; u < amount_u; u++) {
+                float y = yCoordinate(u);
+                heights[v][u] = heightAt(x, y);
             }
         }
     }
@@ -37,13 +37,13 @@ class Terrain {
         RobotRace.setMaterial(gl, new float[] {.8f, .8f, .8f, 1f}, 20, "metal");
         
         
-        for(int y = 0; y < amount_y - 1; y++) {
+        for(int u = 0; u < amount_u - 1; u++) {
             gl.glBegin(gl.GL_TRIANGLE_STRIP);
             Vector oldPoint1 = new Vector(0,0,0);
             Vector oldPoint2 = new Vector(0,0,0);
-            for(int x = 0; x < amount_x; x++) {
-                Vector point1 = Coordinate(x, y);       // Get the coordinate of the first point.
-                Vector point2 = Coordinate(x, y + 1);   // Get the coordinate of the second point.
+            for(int v = 0; v < amount_v; v++) {
+                Vector point1 = Coordinate(v, u);       // Get the coordinate of the first point.
+                Vector point2 = Coordinate(v, u + 1);   // Get the coordinate of the second point.
                 
                 Vector diagonal = point1.subtract(oldPoint2);           // The line from oldP2 to P1.
                 Vector newHorizontal = point2.subtract(point1);         // The line from P1 to P2.
@@ -65,34 +65,66 @@ class Terrain {
         }
     }
     
+    /**
+     * Sets the normal vector of the upcoming triangles.
+     * 
+     * @param GL2     The JOGL 2 object.
+     * @param Vector  The normal vector which should be applied.
+     */
     public void setNormal(GL2 gl, Vector normal) {
         gl.glNormal3d(normal.x(), normal.y(), normal.z());
     }
-
+    
+    /**
+     * Draws a line part from the previous line end to the specified point.
+     * 
+     * @param GL2       The JOGL 2 object.
+     * @param Vector    The point where the line should be drawn to. 
+     */
     public void drawLineSegment(GL2 gl, Vector point) {
         gl.glVertex3d(point.x(), point.y(), point.z());
     }
     
-    public Vector Coordinate(int x, int y) {
+    /**
+     * Calculates a coordinate for a point. Uses the heights array for the z
+     * coordinate.
+     * 
+     * @param int       The v-th point of which the coordinates should be returned.
+     * @param int       The u-th point of which the coordinates should be returned.     
+     * @return Vector   The coordinates of the point.
+     */
+    public Vector Coordinate(int v, int u) {
         return new Vector(
-                xCoordinate(x),
-                yCoordinate(y),
-                heights[x][y]
+                xCoordinate(v),
+                yCoordinate(u),
+                heights[v][u]
         );
     }
     
-    public float xCoordinate(int x) {
-        return ((2 * x / (float) amount_x) - 1f) * size_x; // Calculate the x coordinate.
-    }
-    
-    public float yCoordinate(int y) {
-        return ((2 * y / (float) amount_y) - 1f) * size_y; // Calculate the y coordinate.
+    /**
+     * Calculates the coordinate of the v-th point.
+     * 
+     * @param float     The value of v.
+     * @return float    The value of the corresponding v-coordinate.
+     */
+    public float xCoordinate(float v) {
+        return ((2 * v / (float) amount_v) - 1f) * size_x; // Calculate the v coordinate.
     }
     
     /**
-     * Computes the elevation of the terrain at (x, y).
+     * Calculates the coordinate of the u-th point.
+     * 
+     * @param float     The value of u.
+     * @return float    The value of the corresponding u-coordinate.
      */
-    public float heightAt(float x, float y) {
-        return (float) (0.6 * cos(0.3 * x + 0.2 * y) + 0.4 * cos(x - 0.5 * y));
+    public float yCoordinate(float u) {
+        return ((2 * u / (float) amount_u) - 1f) * size_y; // Calculate the u coordinate.
+    }
+    
+    /**
+     * Computes the elevation of the terrain at (v, u).
+     */
+    public float heightAt(float v, float u) {
+        return (float) (0.6 * cos(0.3 * v + 0.2 * u) + 0.4 * cos(v - 0.5 * u));
     }
 }
